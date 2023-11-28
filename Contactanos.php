9d0a08a44e33bd1490898fa1f16ff24199af0445
Sebas <saebrdz99hdz@outlook.com>
2023-11-27 19:23:10 -0600

    pie de pagina

--- Contactanos.php
+++ Contactanos.php
@@ -1,3 +1,29 @@
 <?php
     echo    "contactanos:" ;
 ?>
+<header class="header">
+
+<a href="#" class="logo"> <i class="fas fa-shopping-cart"></i> The void zone </a>
+
+<nav class="navbar">
+    <a href="#home">Home</a>
+    <a href="#products">Productos</a>
+    <a href="#featured">Destacados</a>
+    <a href="#review">Revisar</a>
+    <a href="#contact">Contacto</a>
+    <a href="#blogs">Blogs</a>
+</nav>
+
+<div class="icons">
+    <div id="menu-btn" class="fas fa-bars"></div>
+    <div id="search-btn" class="fas fa-search"></div>
+    <a href="#" class="fas fa-shopping-cart"></a>
+    <a href="#" class="fas fa-heart"></a>
+</div>
+
+<form action="" class="search-form">
+    <input type="search" name="" placeholder="search here..." id="search-box">
+    <label for="search-box" class="fas fa-search"></label>
+</form>
+
+</header>
