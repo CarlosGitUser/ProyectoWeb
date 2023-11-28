bd26e650ff0ceec6914bb17c3e5a84a0f8f35b57
Jazmin Martinez <jazminmartinez55371@gmail.com>
2023-11-27 19:32:07 -0600

    agregue pie de pagian en cada pagina

--- Contactanos.php
+++ Contactanos.php
@@ -1,6 +1,14 @@
 <?php
     echo    "contactanos:" ;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <header class="header">
 
 <a href="#" class="logo"> <i class="fas fa-shopping-cart"></i> The void zone </a>
@@ -27,3 +35,67 @@
 </form>
 
 </header>
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Sobre nosotros</h3>
+            <p>Viviendo la Aventura en cada Fotograma, Transformamos Sueños en Acción!</p>
+        </div>
+
+        <div class="box">
+            <h3>Categoria</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Terror </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Romance </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Accion </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Ciencia Ficcion </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Comedia </a>
+        </div>
+
+        <div class="box">
+            <h3>Enlaces Rapidos</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Home </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Productos </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Destacados </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Revisar </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Contacto </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Mi pedido </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Mi cuenta </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Mi listado </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Vende ahora </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> Nuevas ofertas </a>
+        </div>
+
+    </div>
+
+    <div class="share">
+        <a href="https://facebook.com/freewebsitecode" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-instagram"></a>
+    </div>
+
+    <div class="credit"> &copy; copyright @ 2021 by <span><a href="">sitio de peliculas</a></span> </div>
+    
+</section>
+
+<!-- footer section ends -->
+</body>
+</html>
+
