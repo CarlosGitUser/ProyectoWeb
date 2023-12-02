5ecbeea43d26fdff84f3013127e68770e5961a81
Sebas <saebrdz99hdz@outlook.com>
2023-12-02 14:14:01 -0600

    a ver si jala el tamaño

--- Vision.php
+++ Vision.php
@@ -36,7 +36,7 @@
     </article>
 
     <article id="mision">
-        <img src="image/misionPROGRA.jpg" alt="" width="300">
+        <img src="image/misionPROGRA.jpg" alt="" width="500">
         <h2>Nuestra Misión</h2>
         <p>La misión de Void Zone es proporcionar a los aficionados del cine y la colección un refugio donde sus pasiones puedan florecer
         Buscamos constantemente innovar en la forma en que los aficionados disfrutan y comparten su amor por el entretenimiento.
@@ -46,7 +46,7 @@
     </article>
 
     <article id="objetivo">
-        <img src="image/objetivosPROGRA.jpg" alt="" width="300">
+        <img src="image/objetivosPROGRA.jpg" alt="" width="500">
         <h2>Nuestro Objetivo</h2>
         <p>El objetivo central de Void Zone es convertirse en el destino predilecto para los entusiastas del cine y la colección, 
             proporcionando una experiencia integral que va más allá de la simple adquisición de productos.
