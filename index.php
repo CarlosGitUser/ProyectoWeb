1701d0cf64b97cf3abf04298c1bb3f2d99c6ada0
Jazmin Martinez <jazminmartinez55371@gmail.com>
2023-12-02 12:46:49 -0600

    cambie el footer en otro archivo aparte

--- index.php
+++ index.php
@@ -17,41 +17,7 @@
 </head>
 <body>
     
-<!-- header section starts  -->
-
-<header class="header">
-
-    <a href="#" class="logo">
-    <img style=" max-width: 80%; display: block; width:170px; height: 110px;" src="image/logo1.png" alt="The void zone">
-    </a>
-
-    <nav class="navbar">
-        <a href="index.php">Home</a>
-        <a href="tienda.php">Productos</a>
-        <a href="Vision.php">Acerca De</a>
-        <a href="Contactanos.php">Contacto</a>
-        <a href="Ayuda.php">Ayuda</a>
-    </nav>
-
-    <div class="icons">
-        <div id="menu-btn" class="fas fa-bars"></div>
-        <div id="search-btn" class="fas fa-search"></div>
-        <a href="#" class="fas fa-shopping-cart"></a>
-        <a href="#" class="fas fa-heart"></a>
-        <a href="#" class="fas fa-user"></a>
-    </div>
-
-    <form action="" class="search-form">
-        <input type="search" name="" placeholder="search here..." id="search-box">
-        <label for="search-box" class="fas fa-search"></label>
-    </form>
-
-</header>
-
 <?php include "header.php"; ?>
-
-
-<!-- header section ends -->
 
 <!-- home section starts  -->
 
@@ -874,50 +840,7 @@
 
 <!-- footer section starts  -->
 
-<section class="footer">
-
-    <div class="box-container">
-
-        <div class="box">
-            <h3>Nuestro lema</h3>
-            <p>¡Transforma tu mundo cinéfilo con nuestra colección de películas y figuras de acción extraordinarias!</p>
-        </div>
-
-        <div class="box">
-            <h3>Categorias</h3>
-            <a href="tienda.php"> <i class="fas fa-arrow-right"></i> Peliculas </a>
-            <a href="tienda.php"> <i class="fas fa-arrow-right"></i> Figuras de acción </a>
-        </div>
-
-        <div class="box">
-            <h3>Enlaces Rapidos</h3>
-            <a href="index.php"> <i class="fas fa-arrow-right"></i> Home </a>
-            <a href="tienda.php"> <i class="fas fa-arrow-right"></i> Productos </a>
-            <a href="Vision.php"> <i class="fas fa-arrow-right"></i> Acerca de </a>
-            <a href="Contactanos.php"> <i class="fas fa-arrow-right"></i> Contacto </a>
-            <a href="Ayuda.php"> <i class="fas fa-arrow-right"></i> Ayuda </a>
-        </div>
-
-        <div class="box">
-            <h3>Extra links</h3>
-            <a href="#"> <i class="fas fa-arrow-right"></i> Mi pedido </a>
-            <a href="#"> <i class="fas fa-arrow-right"></i> Mi cuenta </a>
-            <a href="#"> <i class="fas fa-arrow-right"></i> Mi listado </a>
-        </div>
-
-    </div>
-
-    <div class="share">
-        <a href="https://facebook.com/freewebsitecode" class="fab fa-facebook-f"></a>
-        <a href="#" class="fab fa-twitter"></a>
-        <a href="#" class="fab fa-pinterest"></a>
-        <a href="#" class="fab fa-linkedin"></a>
-        <a href="#" class="fab fa-instagram"></a>
-    </div>
-
-    <div class="credit"> &copy; copyright @ 2021 by <span><a href="">sitio de peliculas y figuras de accion</a></span> </div>
-    
-</section>
+<?php include "footer.php"; ?>
 
 <!-- footer section ends -->
 
