ea030e88bd60731408ab9d0399af62bba390e18e
Sebas <saebrdz99hdz@outlook.com>
2023-12-04 19:48:22 -0600

    modificar

--- cambios.php
+++ cambios.php
@@ -82,6 +82,9 @@ if(isset($_POST['subir'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="image/logo.png">
+    <link rel="stylesheet" href="css/stylescambios.css">
 </head>
 <body>
     <h2>Modificar Cuentas</h2>
