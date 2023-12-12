681ef07cdf82124d309c72c0d40a7a7972f13d4c
jeruvalz <ruvalcabalozanoj@gmail.com>
2023-12-11 18:15:57 -0600

    post de insta

--- footer.php
+++ footer.php
@@ -67,7 +67,7 @@
         <a href="#" class="fab fa-twitter"></a>
         <a href="#" class="fab fa-pinterest"></a>
         <a href="#" class="fab fa-linkedin"></a>
-        <a href="#" class="fab fa-instagram"></a>
+        <a href="https://www.instagram.com/thevoidzon3/" class="fab fa-instagram"></a>
     </div>
 
     <div class="credit"> &copy; copyright @ 2023 by <span><a href="">sitio de películas y figuras de acción</a></span> </div>
--- php/actualizarCarrito.php
+++ php/actualizarCarrito.php
@@ -1,35 +1,33 @@
 <?php
-session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tienda";
 
-if (isset($_SESSION['id_usuario']) && isset($_POST['id_producto']) && isset($_POST['cantidad']) && isset($_POST['precio'])) {
-    $id_usuario = $_SESSION['id_usuario'];
-    $id_producto = $_POST['id_producto'];
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("La conexión falló: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_producto = $_POST['id'];
     $nueva_cantidad = $_POST['cantidad'];
-    $nuevo_precio = $_POST['precio'];
 
-    // Actualizar la cantidad y el precio en la tabla 'carrito' en la base de datos
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "tienda";
+    // Realizar la actualización en la base de datos
+    $sql_actualizar = "UPDATE carrito SET cantidad = $nueva_cantidad WHERE id_prod = $id_producto";
+    $result_actualizar = $conn->query($sql_actualizar);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("La conexión falló: " . $conn->connect_error);
-    }
-
-    // Actualizar la cantidad y el precio en el carrito
-    $sql = "UPDATE carrito SET cantidad = $nueva_cantidad, monto = $nuevo_precio WHERE id_usr = $id_usuario AND id_prod = $id_producto";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "La cantidad y el precio se actualizaron correctamente en la base de datos";
+    if ($result_actualizar) {
+        // La actualización fue exitosa
+        echo json_encode(['success' => true]);
     } else {
-        echo "Error al actualizar la cantidad y el precio: " . $conn->error;
+        // La actualización falló
+        echo json_encode(['success' => false, 'error' => $conn->error]);
     }
-
-    $conn->close();
 } else {
-    echo "Error: Datos faltantes";
+    // Si no es una solicitud POST, redirigir o manejar según sea necesario
 }
 ?>
